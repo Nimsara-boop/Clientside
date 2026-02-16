3ff7ee1d7b13163fecd5669d6304e70a41b2b4f1
Pugsy <nimsarakarunaratne@gmail.com>
2026-02-16 21:58:10 +0530

    Created an actually communicating client and server

--- Echo/src/main/java/com/mycompany/echo/EchoClient.java
+++ Echo/src/main/java/com/mycompany/echo/EchoClient.java
@@ -4,6 +4,12 @@
 
 package com.mycompany.echo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  *
  * @author Nimsara
@@ -11,6 +17,20 @@ package com.mycompany.echo;
 public class EchoClient {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String host = "localhost";
+        int port = 6666;
+        
+        try(
+        Socket socket = new Socket(host, port);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));){
+            
+                out.println("Hellow Distributed System");
+                String response = in.readLine();
+                System.out.println("Server replied: "+response);
+        
+    
+        }catch(IOException e){
+            System.out.println("Connection refused");}
     }
 }
--- Echo/src/main/java/com/mycompany/echo/EchoServer.java
+++ Echo/src/main/java/com/mycompany/echo/EchoServer.java
@@ -4,13 +4,42 @@
  */
 package com.mycompany.echo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
  * @author Nimsara
  */
 public class EchoServer {
-   
+    public static void main(String[] args) {
+        try{
+        ServerSocket serverSocket = new ServerSocket(6666);
+        System.out.println("Waiting for client");
+
+        
+        while (true){
+            Socket clientSocket = serverSocket.accept();
+            
+            PrintWriter out= new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            
+            String inputline;
+            while ((inputline = in.readLine()) != null ) {
+                    out.println( "Echo: "+inputline);
+            }
+        
+        }
+        }
+        
+        catch (Exception e){
+            System.out.println("Problem!");
+        }
+    }
     
 }
