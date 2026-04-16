3b8adb1bb551cf7133b492367d5fb9274904366d
Pugsy <nimsarakarunaratne@gmail.com>
2026-04-16 23:28:52 +0530

    added the test result files

---
+++ mock/src/main/java/com/mycompany/mock/Mock.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mock;
+
+/**
+ *
+ * @author Nimsara
+ */
+public class Mock {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
